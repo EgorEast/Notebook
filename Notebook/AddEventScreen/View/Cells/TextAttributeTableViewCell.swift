247df918eaa1dad83d871c057270e214d2b218b3
NoName <NoMail>
2022-01-13 17:54:16 +0300

    added empty class for textfields for adding event

---
+++ Notebook/AddEventScreen/View/Cells/TextAttributeTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TextAttributeTableViewCell.swift
+//  Notebook
+//
+//  Created by Egor Slobodskoy on 13.01.2022.
+//
+
+import UIKit
+
+final class TextAttributeTableViewCell: UITableViewCell {
+
+    
+
+}
